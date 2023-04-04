63fc29a0259c81e12bc975c3684b65bb7992d74b
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-04-04 16:11:15 +0100

    task 3 complete

--- part-3/calculator.test.js
+++ part-3/calculator.test.js
@@ -1,1 +1,27 @@
+import { test, expect } from "@jest/globals";
+import {calculate} from './calculator.js';
 
+// For example, you could write a test that checks whether the add function returns 3 when passed 1 and 2 as arguments.
+test ("Add Function test", () => {
+    const actual = calculate("+", 1 , 2)
+    const expected = 3
+expect(actual).toBe(expected);
+});
+
+test ("Subtract Function test", () => {
+    const actual = calculate("-", 10 , 6)
+    const expected = 4
+expect(actual).toBe(expected);
+});
+
+test ("Multiply Function test", () => {
+    const actual = calculate("*", 6 , 6)
+    const expected = 36
+expect(actual).toBe(expected);
+});
+
+test ("Divide Function test", () => {
+    const actual = calculate("/", 50 , 10)
+    const expected = 5
+expect(actual).toBe(expected);
+});
